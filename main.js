1ca48a58ce2df8cb851787ab3547e26f74f6611a
drishti0212 <161376483+drishti0212@users.noreply.github.com>
2024-10-17 12:49:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontent Developer", "Software Developer", "Content Creator"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
